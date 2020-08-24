c990cf7cf39546da15495962bfcf0ad7c718c6a1
Alex Gardner <a.gardner.dev@gmail.com>
2020-08-24 04:01:33 -0400

    set consistent color mode
    consistent color mode with localstorage API

--- main.js
+++ main.js
@@ -6,37 +6,61 @@ const lightDarkText = document.querySelector('#light-dark-text')
 const serviceHolderBGIMG = document.querySelector('#service-holder-bg-img')
 const serviceItemList = document.querySelectorAll('.service-item');
 
+// Default Mode to saved local storage
+// localStorage.setItem('colorScheme', 'dark')
 
-(function setLightMode() {
-    lightDarkText.innerHTML = "Dark"
-    serviceHolderBGIMG.src = './assets/nastuh-abootalebi-yWwob8kwOCk-unsplash.jpg'
+let colorScheme = localStorage.getItem('colorScheme') || 'light'
+
+function setDefaultMode(modeChoice) {
+    if (modeChoice === 'light') {
+        localStorage.setItem('colorScheme', 'light')
+        // pageRoot.classList.replace('dark', 'light')
+        // lightDarkText.innerHTML = "Dark"
+    } else if (modeChoice === 'dark') {
+        localStorage.setItem('colorScheme', 'dark')
+        // pageRoot.classList.replace('light', 'dark')
+        // lightDarkText.innerHTML = "Light"
+    }
+}
+
+(function readDefaultMode() {
+    console.log(colorScheme)
+    if (colorScheme === 'light') {
+        pageRoot.classList.add('light')
+        lightDarkText.innerHTML = "Dark"
+        if (!serviceHolderBGIMG) {return} else {serviceHolderBGIMG.src = './assets/nastuh-abootalebi-yWwob8kwOCk-unsplash.jpg'}
+        // serviceHolderBGIMG.src = './assets/nastuh-abootalebi-yWwob8kwOCk-unsplash.jpg'
+    } else if (colorScheme === 'dark') {
+        pageRoot.classList.add('dark')
+        lightDarkText.innerHTML = "Light"
+        if (!serviceHolderBGIMG) {return} else {serviceHolderBGIMG.src = './assets/jamie-street-dO9zecIYnWA-unsplash.jpg'}
+        // serviceHolderBGIMG.src = './assets/jamie-street-dO9zecIYnWA-unsplash.jpg'
+    }
 })();
-
-// if (pageRoot.classList.contains('dark')) {
-//     lightDarkText.innerHTML = "Light"
-// } else if (pageRoot.classList.contains('light')) {
-//     lightDarkText.innerHTML = "Dark"
-// }
+        
 
 function toggleMenuVisible() {
     menuNav.classList.toggle('visible')
 }
 function toggleLightDarkMode() {
+    console.log(colorScheme)
     if (pageRoot.classList.contains('dark')) {
+        setDefaultMode('light')
         pageRoot.classList.replace('dark', 'light')
         lightDarkText.innerHTML = "Dark"
+        if (!serviceHolderBGIMG) {return} else {serviceHolderBGIMG.src = './assets/nastuh-abootalebi-yWwob8kwOCk-unsplash.jpg'}
+        // serviceHolderBGIMG.src = './assets/nastuh-abootalebi-yWwob8kwOCk-unsplash.jpg'        
     }
     else if (pageRoot.classList.contains('light')) {
+        setDefaultMode('dark')
         pageRoot.classList.replace('light', 'dark')
         lightDarkText.innerHTML = "Light"
-    }
-    if (pageRoot.classList.contains('light')) {
-        serviceHolderBGIMG.src = './assets/nastuh-abootalebi-yWwob8kwOCk-unsplash.jpg'
-    }
-    else if (pageRoot.classList.contains('dark')) {
-        serviceHolderBGIMG.src = './assets/jamie-street-dO9zecIYnWA-unsplash.jpg'
+        if (!serviceHolderBGIMG) {return} else {serviceHolderBGIMG.src = './assets/jamie-street-dO9zecIYnWA-unsplash.jpg'}
+        // serviceHolderBGIMG.src = './assets/jamie-street-dO9zecIYnWA-unsplash.jpg'
     }
 }
+
+// ---------------------------------------------
 
 
 serviceItemList.forEach(serviceItem => {
